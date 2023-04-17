dad1e80839f59bb25f824531e692a6df3028a28d
Janishnehyan03 <nehyanjanish@gmail.com>
2023-04-18 00:22:32 +0530

    logout added

--- controllers/authController.js
+++ controllers/authController.js
@@ -196,3 +196,11 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.logout = (req, res) => {
+  res.cookie("jwt", "logged out", {
+    expires: new Date(Date.now() + 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: "success" });
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signUp, login, verifyToken } = require("../controllers/authController");
+const { signUp, login, verifyToken, logout } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
+router.get("/logout", logout);
 router.post("/verify-token", verifyToken);
 
 module.exports = router;
